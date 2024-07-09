0569d196013bf31fb57574329a4492bc19b21d74
Huanchen Zhai <hczhai@ucla.edu>
2024-07-08 18:52:13 -0700

    init

---
+++ unit_test/test_fft.cpp
@@ -0,0 +1,37 @@
+
+#include "block2_core.hpp"
+#include <gtest/gtest.h>
+
+using namespace block2;
+
+class TestFFT : public ::testing::Test {
+  protected:
+    static const int n_tests = 1000;
+    void SetUp() override { Random::rand_seed(0); }
+    void TearDown() override {}
+};
+
+TEST_F(TestFFT, TestFFT) {
+    for (int i = 0; i < n_tests; i++) {
+        int n;
+        if (i < n_tests * 50 / 100)
+            n = Random::rand_int(0, 12);
+        else if (i < n_tests * 99 / 100)
+            n = Random::rand_int(1, 1000);
+        else
+            n = Random::rand_int(1, 5000);
+        FFT fft;
+        DFT dft;
+        vector<complex<double>> arr(n), arx;
+        Random::fill<double>((double *)arr.data(), n * 2);
+        arx = arr;
+        fft.fft(arx.data(), n, true);
+        dft.fft(arr.data(), n, true);
+        for (int j = 0; j < n; j++)
+            ASSERT_TRUE(abs(arr[i] - arx[i]) < 1E-12 + 1E-5 * abs(arx[i]));
+        fft.fft(arx.data(), n, false);
+        dft.fft(arr.data(), n, false);
+        for (int j = 0; j < n; j++)
+            ASSERT_TRUE(abs(arr[i] - arx[i]) < 1E-12 + 1E-5 * abs(arx[i]));
+    }
+}
